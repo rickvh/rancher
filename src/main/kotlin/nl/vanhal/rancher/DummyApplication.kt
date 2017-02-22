9afc1c77baf94f950b4b75a7cf685b4ec675708b
Rick van Hal <rick.van.hal@topicus.nl>
2017-02-22 11:09:35 +0100

    experimental had dependency issues

--- src/main/kotlin/nl/vanhal/rancher/DummyApplication.kt
+++ src/main/kotlin/nl/vanhal/rancher/DummyApplication.kt
@@ -2,7 +2,6 @@ package nl.vanhal.rancher
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 
 @EnableAutoConfiguration
